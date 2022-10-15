6eba1e452c54686bed349842ea5f50a091f01c09
abuchijuliet <abuchijuliet44@gmail.com>
2022-10-15 04:52:31 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,6 +15,10 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
+		else if (n < 0)
+		{
+		printf("%d is negative\n", n);
+	}
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	return (0);
