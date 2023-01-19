3a2084c6943524242fd861178d81720060afed7b
abuchijuliet <abuchijuliet44@gmail.com>
2023-01-19 16:51:30 +0000

    main.h

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _isupper - checks Uppercase or Lowercase
+*@c:the value of c will be checked
+*Return: 0 for lowercase or 1 for uppercase
+*/
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
